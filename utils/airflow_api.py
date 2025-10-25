3232a4b2f737d0530513064eea8da43d84221e0d
MartinKratkyProject <martin.kratky.job@gmail.com>
2025-10-25 16:42:32 +0200

    trigger with api

--- utils/airflow_api.py
+++ utils/airflow_api.py
@@ -1,4 +1,5 @@
 import requests
+from datetime import datetime, timezone
 
 localhost = 'http://localhost:8080/'
 api_endpoint = '/api/v2'
@@ -35,7 +36,30 @@ response = get_dags(localhost + api_endpoint + '/dags')
 
 dags = response.json()['dags']
 
-for dag in dags:
-    print(dag['dag_id'])
-    print(dag['description'])
+
+def trigger_dag(dag_id):    
+    token = get_jwt_token(localhost + auth_endpoint)
+
+    payload = {
+        "conf": {},
+        "logical_date": datetime.now(timezone.utc).isoformat()
+    }
+
+    print(datetime.now(timezone.utc).isoformat())
+
+    headers = {
+        'Authorization': 'Bearer ' + token,
+        'Content-Type': 'application/json',
+    }
+
+    response = requests.post(
+        localhost + api_endpoint + '/dags/' + dag_id + '/dagRuns', 
+        json=payload, 
+        headers=headers
+    )
+
+    return response
+
+print("triggering dag")
+print(trigger_dag('test_dag_1'))
 
